66a9fbc8c8fa7861be2167ab748c9b1dad6c640b
Wolf <xy1157@163.com>
2018-04-01 02:51:25 +0800

    上传随机音乐播放器

---
+++ autoplay/autoplay.js
@@ -0,0 +1,32 @@
+var audio = document.getElementById('audio');
+var text = document.getElementById('text');
+var resault = 0;
+var geturl = "https://music.163.com/song/media/outer/url?id = ";
+var ext = ".mp3"
+var music = [[],[]];
+list();
+
+function random(){
+	var num = Math.round(Math.random() * (music[0].length-1));
+	while(num == resault){
+		num = Math.round(Math.random() * (music[0].length-1));
+	}
+	resault = num;
+	return num;
+}
+
+function play(){
+	if(music[0].length == music[1].length){
+		var ran = random();
+		audio.setAttribute('src',geturl + music[0][ran] + ext);
+		text.innerHTML = ran + 1 + " - " + music[1][ran];
+	}else{
+		text.innerHTML = "ERROR";
+	}
+}
+
+audio.addEventListener('ended',function(){  
+	play();
+},false);
+
+play();
